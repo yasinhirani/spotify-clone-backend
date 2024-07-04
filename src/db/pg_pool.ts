b844b024caac1e8555dafcb48d55243422df300a
Yasin Hirani <yasin.hirani@1rivet.com>
2024-07-04 18:35:58 +0530

    Fix : Connection string for database

--- src/db/pg_pool.ts
+++ src/db/pg_pool.ts
@@ -2,12 +2,7 @@ import { Pool } from "pg";
 
 const pool = () => {
   return new Pool({
-    // connectionString: process.env.POSTGRES_URL,
-    database: "TuneTide",
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: "Admin@123"
+    connectionString: process.env.POSTGRES_URL,
   });
 };
 
