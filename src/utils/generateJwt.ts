375227a388289a4729fcd2084eeb0b4740629bd7
Yasin Hirani <60186127+yasinhirani@users.noreply.github.com>
2024-07-05 13:00:18 +0530

    Fix : Increased JWT expiry time from 1 min to 5 min

--- src/utils/generateJwt.ts
+++ src/utils/generateJwt.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const generateJsonWebToken = (email: string) => {
-  const token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET!, {expiresIn: "1 min"});
+  const token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET!, {expiresIn: "5 min"});
   return token;
 };
 
